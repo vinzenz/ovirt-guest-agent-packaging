f78722002b7d3c984741349b3fada531a9245f07
Vinzenz Feenstra <vfeenstr@redhat.com>
2014-04-30 12:30:13 +0000

    Adding debian prepare script
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>

---
+++ helpers/prepare.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+./helpers/get_latest.sh
+
+LATEST_VERSION="`cat LATEST_VERSION`"
+FOLDER_NAME="ovirt-guest-agent-${LATEST_VERSION}"
+SOURCE_NAME="ovirt-guest-agent-${LATEST_VERSION}.tar.bz2"
+ORIG_NAME="ovirt-guest-agent_${LATEST_VERSION}.orig.tar.bz2"
+
+/bin/tar xf ${SOURCE_NAME}
+/bin/mv ${SOURCE_NAME} ${ORIG_NAME}
+/bin/rm -rf ${FOLDER_NAME}/debian
+/bin/cp -a debian ${FOLDER_NAME}
+
+./helpers/create_patches.sh
