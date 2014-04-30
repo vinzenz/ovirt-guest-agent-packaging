8fceb2682ef2beb88bb4ded00f96ed62eeb02ad4
Vinzenz Feenstra <vfeenstr@redhat.com>
2014-04-30 13:13:07 +0000

    Extra sources and apply patches
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>

---
+++ helpers/apply-patches.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+LATEST_VERSION=`cat LATEST_VERSION`
+SOURCE_DIR="ovirt-guest-agent-${LATEST_VERSION}"
+HOOK_DIR=./helpers/hooks/apply-patch
+START_DIR=`pwd`
+
+apply_patch() {
+    PATCH_PATH=$1
+    PATCH_FILE_NAME=$2
+
+    if [ -f ${HOOK_DIR}/before ]; then
+        ${HOOK_DIR}/before "${PATCH_PATH}" "${PATCH_FILE_NAME}" || return 0
+    fi
+    echo Attempt to apply patch $START_DIR/$PATCH_PATH/$PATCH_FILE_NAME
+    cd $START_DIR/$SOURCE_DIR
+    patch -p1 -l < $START_DIR/$PATCH_PATH/$PATCH_FILE_NAME
+    PATCH_RESULT=$?
+    echo patch result is $PATCH_RESULT
+    if [  ${PATCH_RESULT} != 0 ]; then
+        echo Applying $PATCH_FILE_NAME failed. Patch returned ${PATCH_RESULT}
+        cd ..
+        return 1
+    fi
+    cd $START_DIR
+
+    if [ -f ${HOOK_DIR}/after ]; then
+        ${HOOK_DIR}/after "${PATCH_PATH}" "${PATCH_FILE_NAME}" || return 1
+    fi
+    return 0
+}
+
+for PATCH in `ls patches/*patch`; do
+    apply_patch "patches" "`basename $PATCH`"
+    APPLY_PATCH_RESULT=$?
+    if [ ${APPLY_PATCH_RESULT} != 0 ]; then
+        echo "Aborting after $PATCH"
+        return 1
+    fi
+done
---
+++ helpers/hooks/prepare/before.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+LATEST_VERSION="`cat LATEST_VERSION`"
+
+if [ -d "./extra-sources/ovirt-guest-agent-${LATEST_VERSION}" ]; then
+    cp -a ./extra-sources/ovirt-guest-agent-${LATEST_VERSION}/* "ovirt-guest-agent-${LATEST_VERSION}"
+fi
