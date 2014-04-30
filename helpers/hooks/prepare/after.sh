36e698858b8660c2b9ad74cc5101e0ad4045fccb
Vinzenz Feenstra <vfeenstr@redhat.com>
2014-04-30 13:19:01 +0000

    Moved and modified debian script to after hook
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>

--- helpers/hooks/prepare/after.sh
+++ helpers/hooks/prepare/after.sh
@@ -7,9 +7,6 @@ FOLDER_NAME="ovirt-guest-agent-${LATEST_VERSION}"
 SOURCE_NAME="ovirt-guest-agent-${LATEST_VERSION}.tar.bz2"
 ORIG_NAME="ovirt-guest-agent_${LATEST_VERSION}.orig.tar.bz2"
 
-/bin/tar xf ${SOURCE_NAME}
 /bin/mv ${SOURCE_NAME} ${ORIG_NAME}
 /bin/rm -rf ${FOLDER_NAME}/debian
 /bin/cp -a debian ${FOLDER_NAME}
-
-./helpers/create_patches.sh
